c874fcaf8bea042a31933843b4c7e2d56b518acb
Luis <luis.15598@hotmail.com>
2024-08-14 12:13:13 -0500

    first

--- src/app/componentes/informacion-beca/informacion-beca.component.ts
+++ src/app/componentes/informacion-beca/informacion-beca.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-informacion-beca',
   templateUrl: './informacion-beca.component.html',
   styleUrl: './informacion-beca.component.css'
 })
-export class InformacionBecaComponent {
+export class InformacionBecaComponent implements OnInit {
+  StudentArray: any[] = [];
+  isResultLoaded = false;
+
+  constructor(private http: HttpClient) {
+   
+  }
+
+  ngOnInit(): void {
+
+    
+  }
+
+  getAllStudent() {
+    this.http.get("http://localhost:3000/solicitudes")
+      .subscribe((resultData: any) => {
+        this.isResultLoaded = true;
+        console.log(resultData.data);
+        this.StudentArray = resultData.data;
+      });
+      console.log(this.StudentArray)
+  }
 
 }
+
+
