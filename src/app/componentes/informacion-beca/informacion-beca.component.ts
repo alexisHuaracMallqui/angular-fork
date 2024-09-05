c675488d1f9ed5eb380e6a7a2edbe9349c906c76
Luis <luis.15598@hotmail.com>
2024-09-05 14:22:50 -0500

    twelve

--- src/app/componentes/informacion-beca/informacion-beca.component.ts
+++ src/app/componentes/informacion-beca/informacion-beca.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { SolicitudService } from '../../servicios/solicitud/solicitud.service';
 import { firstValueFrom, forkJoin, map, Observable } from 'rxjs';
@@ -19,7 +19,6 @@ import { Router } from '@angular/router';
   styleUrl: './informacion-beca.component.css'
 })
 export class InformacionBecaComponent implements OnInit {
-
   solicitud: any;
   ciclosMallaLista?: CicloMalla[];
   cursoMallaLista?: CursoMalla[];
@@ -28,55 +27,38 @@ export class InformacionBecaComponent implements OnInit {
   url_foto_estudiante = new FormData();
   url_doc_contrato = new FormData();
   id_malla_curricular = new FormData();
-  formInfoPeriodo = this.formBuilder.group({
-    nombre_completo: [{ value: '', disabled: true }],
-    dni: [{ value: '', disabled: true }],
-    institucion_nombre: [{ value: '', disabled: true }],
-    fecha_inicio: ['', [Validators.required]],
-    fecha_fin_estimada: ['', [Validators.required]]
-  });
+
+  nombre_completo: string = '';
+  dni: string = '';
+  institucion_nombre: string = '';
+  fecha_inicio: string = '';
+  fecha_fin_estimada: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private solicitudService: SolicitudService,
     private http: HttpClient,
     private mallaCurricularService: MallaCurricularService,
-    private router: Router
-  ) {
-
-  }
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
     this.solicitudService.getSolicitudData().subscribe(data => {
       this.solicitud = data;
       if (this.solicitud) {
-        this.formInfoPeriodo.patchValue({
-          dni: this.solicitud.dni,
-          nombre_completo: this.solicitud.nombre_completo,
-          institucion_nombre: this.solicitud.institucion_nombre,
-          fecha_inicio: this.solicitud.fecha_inicio,
-          fecha_fin_estimada: this.solicitud.fecha_fin_estimada
-        });
-
-        const fecha_inicial = this.solicitud.fecha_inicio;
-        const fecha_estimada = this.solicitud.fecha_fin_estimada;
-
-        const formattedDateInitial = this.formatDateForInput(fecha_inicial);
-        const formattedDateFinal = this.formatDateForInput(fecha_estimada);
-
-        this.formInfoPeriodo.patchValue({
-          fecha_inicio: formattedDateInitial,
-          fecha_fin_estimada: formattedDateFinal
-        });
-
-        //Obteniendo Malla por ID Solicitud
+        this.nombre_completo = this.solicitud.nombre_completo;
+        this.dni = this.solicitud.dni;
+        this.institucion_nombre = this.solicitud.institucion_nombre;
+        this.fecha_inicio = this.formatDateForInput(this.solicitud.fecha_inicio);
+        this.fecha_fin_estimada = this.formatDateForInput(this.solicitud.fecha_fin_estimada);
         this.getMallaCiclos();
 
       } else {
         console.error('Solicitud data is not available');
       }
     });
-
+    this.cdr.detectChanges();
   }
 
   onFileChange_url_foto_estudiante(event: Event) {
@@ -103,43 +85,73 @@ export class InformacionBecaComponent implements OnInit {
     }
   }
 
-
   updateInformacionBecario() {
 
-    const cargaArchivos = [
-      this.http.post('http://localhost:3000/upload', this.url_foto_estudiante),
-      this.http.post('http://localhost:3000/upload', this.id_malla_curricular),
-      this.http.post('http://localhost:3000/upload', this.url_doc_contrato)
-    ]
+    const cargaArchivos: Observable<any>[] = [];
 
-    forkJoin(cargaArchivos).subscribe({
-      next: (responses: any[]) => {
-        this.solicitud.url_foto_estudiante = responses[0].url;
-        this.solicitud.id_malla_curricular = responses[1].url;
-        this.solicitud.url_doc_contrato = responses[2].url;
+    if (this.url_foto_estudiante.has('file')) {
+      cargaArchivos.push(this.http.post('http://localhost:3000/upload', this.url_foto_estudiante));
+    }
 
+    if (this.id_malla_curricular.has('file')) {
+      cargaArchivos.push(this.http.post('http://localhost:3000/upload', this.id_malla_curricular));
+    }
 
-        this.solicitud.fecha_inicio = this.formInfoPeriodo.get('fecha_inicio')?.value;
-        this.solicitud.fecha_fin_estimada = this.formInfoPeriodo.get('fecha_fin_estimada')?.value;
+    if (this.url_doc_contrato.has('file')) {
+      cargaArchivos.push(this.http.post('http://localhost:3000/upload', this.url_doc_contrato));
+    }
 
-        this.solicitudService.updateSolicitud(this.solicitud).subscribe({
-          next: (response: any) => {
-            console.log('Se actualizó correctamente', response);
-          },
-          error: (error: any) => {
-            console.error('Error al actualizar información', error);
+    if (cargaArchivos.length > 0) {
+      forkJoin(cargaArchivos).subscribe({
+        next: (responses: any[]) => {
+          if (responses.length > 0) {
+            if (this.url_foto_estudiante.has('file')) {
+              this.solicitud.url_foto_estudiante = responses[0].url;
+            }
+            if (this.id_malla_curricular.has('file')) {
+              this.solicitud.id_malla_curricular = responses[1].url;
+            }
+            if (this.url_doc_contrato.has('file')) {
+              this.solicitud.url_doc_contrato = responses[2].url;
+            }
           }
-        });
 
-      },
-      error: (error) => {
-        console.error('Error subiendo archivos', error);
-      }
-    })
+          this.solicitud.fecha_inicio = this.fecha_inicio;
+          this.solicitud.fecha_fin_estimada = this.fecha_fin_estimada;
+
+          this.solicitudService.updateSolicitud(this.solicitud).subscribe({
+            next: (response: any) => {
+              console.log('Se actualizó correctamente', response);
+              this.cdr.detectChanges();
+            },
+            error: (error: any) => {
+              console.error('Error al actualizar información', error);
+            }
+          });
+        },
+        error: (error) => {
+          console.error('Error subiendo archivos', error);
+        }
+      });
+    } else {
+      this.solicitud.fecha_inicio = this.fecha_inicio;
+      this.solicitud.fecha_fin_estimada = this.fecha_fin_estimada;
+
+      this.solicitudService.updateSolicitud(this.solicitud).subscribe({
+        next: (response: any) => {
+          console.log('Se actualizó correctamente', response);
+          this.cdr.detectChanges();
+        },
+        error: (error: any) => {
+          console.error('Error al actualizar información', error);
+        }
+      });
+    }
   }
 
+
   submitForm() {
-    if (this.formInfoPeriodo.valid) {
+    if (this.fecha_inicio && this.fecha_fin_estimada) {
       this.updateInformacionBecario();
     } else {
       console.log('Formulario invalido');
@@ -150,14 +162,12 @@ export class InformacionBecaComponent implements OnInit {
     return moment.utc(dateString).format('YYYY-MM-DD');
   }
 
-
   getMallaCiclos() {
     if (this.solicitud && this.solicitud.id) {
       this.mallaCurricularService.getCiclosMallaBySolicitud(this.solicitud.id).subscribe({
         next: (ciclos: CicloMalla[]) => {
           console.log('Ciclos received:', ciclos);
           this.ciclosMallaLista = ciclos;
-
           this.fetchCursosForCiclos(ciclos);
         },
         error: (error) => {
@@ -168,7 +178,6 @@ export class InformacionBecaComponent implements OnInit {
       console.error('id_solicitud is undefined or not available');
     }
   }
-
 
   fetchCursosForCiclos(ciclos: CicloMalla[]) {
     if (ciclos.length === 0) {
@@ -192,23 +201,6 @@ export class InformacionBecaComponent implements OnInit {
     });
   }
 
-  /*
-  deleteCurso(id_ciclo: number) {
-    if (confirm('Are you sure you want to delete this course?')) {
-      this.mallaCurricularService.deleteCursoMallaByCiclo(id_ciclo).subscribe({
-        next: (response) => {
-          alert('Course deleted successfully!');
-          this.getMallaCiclos();
-        },
-        error: (error) => {
-          console.error('There was an error!', error);
-          alert('An error occurred while deleting the course.');
-        }
-      });
-    }
-  }*/
-
-
   deleteCicloAll(ciclo: CicloMalla): void {
     if (confirm(`¿Estás seguro de que deseas eliminar el ciclo ${ciclo.nombre}?`)) {
       this.mallaCurricularService.deleteCursoMallaByCiclo(ciclo.id).subscribe({
@@ -217,18 +209,16 @@ export class InformacionBecaComponent implements OnInit {
             next: (response) => {
               console.log('CicloMalla deleted successfully:', response);
               this.getMallaCiclos();
-              //Para revisar ese query
-              //this.ciclosMallaLista = this.ciclosMallaLista?.filter(c => c.id !== ciclo.id);
             },
             error: (err) => {
               console.error('Error deleting CicloMalla:', err);
-              alert('Error al eliminar Ciclo')
+              alert('Error al eliminar Ciclo');
             }
           });
         },
         error: (err) => {
           console.error('Error deleting CursoMalla by ciclo:', err);
-          alert('Error al eliminar el curso del ciclo')
+          alert('Error al eliminar el curso del ciclo');
         }
       });
     }
@@ -247,5 +237,5 @@ export class InformacionBecaComponent implements OnInit {
       console.error('No Ciclo ID selected');
     }
   }
-
 }
+
